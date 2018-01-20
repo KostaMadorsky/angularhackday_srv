aa748e5dfe40bb0aead90f1f4b20f427181389e9
madkonst <kmadorsky@yahoo.com>
2018-01-20 17:18:06 +1100

    Add project files.

---
+++ SirnalRServer/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SirnalRServer
+{
+    public class ChatHub: Hub
+    {
+        public async Task JoinRoom(string room)
+        {
+            await Groups.AddAsync(this.Context.ConnectionId, room);
+        }
+
+        public async Task Leave(string room)
+        {
+            await Groups.RemoveAsync(this.Context.ConnectionId, room);
+        }
+    }
+
+}
---
+++ SirnalRServer/Controllers/ChatController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SirnalRServer.Controllers
+{
+    public class Message
+    {
+        public string text { get; set; }
+        public string author { get; set; }
+    }
+
+    public class Input
+    {
+        public string msg { get; set; }
+        public string author { get; set; }
+        public string room { get; set; }
+    }
+
+    [Route("api/messages")]
+    [Produces("application/json")]
+    public class ChatController : Controller
+    {
+        private readonly IHubContext<ChatHub> ctx;
+
+        public ChatController(IHubContext<ChatHub> ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Input input)
+        {
+            await ctx.Clients.Group(input.room).InvokeAsync("messageReceived", new Message {
+                text = input.msg,
+                author = input.author
+            });
+
+            return Ok();
+        }
+
+        
+    }
+}
